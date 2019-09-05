565fdcd4efb8c89dadad50cefda9ad7863a077d0
mariabelenamat <belen.amat29@gmail.com>
2019-09-05 20:46:27 -0300

    Agrego titulo y cards al login del admin

--- frontend-dolce-margarita/src/componentes/loginAdmin/LoginAdmin.js
+++ frontend-dolce-margarita/src/componentes/loginAdmin/LoginAdmin.js
@@ -1,12 +1,41 @@
 import React from 'react';
+
 import './loginAdmin.scss';
 
 export default class LoginAdmin extends React.Component {
     render() {
         return (
-           <div className="login-page">
-               <h1 className="login-title">Bienvenido Admin</h1>
-           </div>
+            <div className="home-page">
+                <div className="container home-title-container">
+                    <h1 className="title"> ¿Qué vas a cargar?</h1>
+                </div>
+
+                <div className="container cards-container">
+                    <div className="columns">
+                        <div className="column">
+                            <div className="card">
+                                <div className="card-content">
+                                    <h1>Figura</h1>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="column">
+                            <div className="card">
+                                <div className="card-content">
+                                    <h1>Huevo</h1>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="column">
+                            <div className="card">
+                                <div className="card-content">
+                                    <h1>Bomboneria</h1>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         )
     }
 }
