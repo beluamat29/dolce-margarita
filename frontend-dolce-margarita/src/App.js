3b23febce5dfecc298301b9ebba05b1f389e4291
belu <belen.amat29@gmail.com>
2019-09-04 21:48:35 -0300

    Pagina simple de bienvenida al admin

--- frontend-dolce-margarita/src/App.js
+++ frontend-dolce-margarita/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import LoginAdmin from "./paginas/LoginAdmin";
+import LoginAdmin from "./componentes/loginAdmin/LoginAdmin";
 
 
 function App() {
---
+++ frontend-dolce-margarita/src/componentes/loginAdmin/LoginAdmin.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './loginAdmin.scss';
+
+export default class LoginAdmin extends React.Component {
+    render() {
+        return (
+           <div className="login-page">
+               <h1 className="login-title">Bienvenido Admin</h1>
+           </div>
+        )
+    }
+}
