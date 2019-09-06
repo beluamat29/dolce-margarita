41979118f5ed04cd8a75c9a31da641da6eb34c82
mariabelenamat <belen.amat29@gmail.com>
2019-09-06 14:56:26 -0300

    Agrego campo para descripcion del producto. Cambio colores de bulma

--- frontend-dolce-margarita/src/componentes/paginaCargaDeDatos/cargaDeDatos.js
+++ frontend-dolce-margarita/src/componentes/paginaCargaDeDatos/cargaDeDatos.js
@@ -16,7 +16,7 @@ export default class CargaDeDatos extends React.Component {
 
                     <p className="subtitle is-4 is-spaced">formato del producto</p>
                     <div className="control">
-                        <div className="select">
+                        <div className="select is-fullwidth">
                             <select>
                                 <option>Figura</option>
                                 <option>Bomboneria</option>
@@ -29,15 +29,22 @@ export default class CargaDeDatos extends React.Component {
                 <div className="field">
                     <p className="subtitle is-4 is-spaced">nombre</p>
                         <div className="control">
-                            <input className="input is-primary" type="text"/>
+                            <input className="input is-danger" type="text"/>
                         </div>
                 </div>
 
                 <div className="field">
                     <p className="subtitle is-4 is-spaced">precio</p>
                         <div className="control">
-                            <input className="input is-primary" type="text"/>
+                            <input className="input" type="text"/>
                         </div>
+                </div>
+
+                <div className="field">
+                    <p className="subtitle is-4 is-spaced">descripción</p>
+                    <div className="control">
+                        <input className="textarea" type="text"/>
+                    </div>
                 </div>
             </div>
         )
