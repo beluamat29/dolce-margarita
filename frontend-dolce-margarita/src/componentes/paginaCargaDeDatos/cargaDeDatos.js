8d79b6c81d3e8d4aa411fc159f57664984c249c0
mariabelenamat <belen.amat29@gmail.com>
2019-09-06 15:46:59 -0300

    Agrego libreria de dropdown para el selector de formato de chocolates.

--- frontend-dolce-margarita/src/componentes/paginaCargaDeDatos/cargaDeDatos.js
+++ frontend-dolce-margarita/src/componentes/paginaCargaDeDatos/cargaDeDatos.js
@@ -1,8 +1,22 @@
 import React from 'react';
-
+import Select from "react-dropdown-select";
 import './cargaDeDatos.scss';
 
 export default class CargaDeDatos extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            tiposDeFormato: [
+                {nombreFormato:'Huevo', valorFormato: 'Huevo'},
+                {nombreFormato:'Figura', valorFormato: 'Figura'},
+                {nombreFormato:'Bomboneria', valorFormato: 'Bomboneria'}
+                ],
+            formatoSeleccionado: undefined
+        }
+
+    }
+
     render() {
         return (
             <div class="home-carga-de-datos ">
@@ -13,37 +27,34 @@ export default class CargaDeDatos extends React.Component {
 
 
                 <div className="field">
-
                     <p className="subtitle is-4 is-spaced">formato del producto</p>
                     <div className="control">
-                        <div className="select is-fullwidth">
-                            <select>
-                                <option>Figura</option>
-                                <option>Bomboneria</option>
-                                <option>Huevo</option>
-                            </select>
-                        </div>
+                        <Select className="formato-chocolate-selector is-fullwidth" labelField="nombreFormato" valueField="valorFormato"
+                                options={this.state.tiposDeFormato}
+                                placeholder={''}
+                                onChange={(formato) => this.setState({formatoSeleccionado: formato})}
+                        />
                     </div>
                 </div>
 
                 <div className="field">
                     <p className="subtitle is-4 is-spaced">nombre</p>
                         <div className="control">
-                            <input className="input is-danger" type="text"/>
+                            <input className="input is-primary" type="text"/>
                         </div>
                 </div>
 
                 <div className="field">
                     <p className="subtitle is-4 is-spaced">precio</p>
                         <div className="control">
-                            <input className="input" type="text"/>
+                            <input className="input is-primary" type="text"/>
                         </div>
                 </div>
 
                 <div className="field">
                     <p className="subtitle is-4 is-spaced">descripción</p>
                     <div className="control">
-                        <input className="textarea" type="text"/>
+                        <input className="textarea is-primary" type="text"/>
                     </div>
                 </div>
             </div>
