207d3864476ed2dca54b8ba32bad427acdb17693
mariabelenamat <belen.amat29@gmail.com>
2019-09-05 21:13:17 -0300

    Agrego Switch para el Routing

--- frontend-dolce-margarita/src/App.js
+++ frontend-dolce-margarita/src/App.js
@@ -1,19 +1,26 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter, Switch, Route, Redirect} from "react-router-dom";
 import LoginAdmin from "./componentes/loginAdmin/LoginAdmin";
 import Home from "./componentes/home/Home";
 
 
 function App() {
   return (
-      <Router>
-        <div>
-          <Route path="/" exact component={Home} />
-            <Route path="/adminlogin" exact component={LoginAdmin} />
-        </div>
-      </Router>
+      <BrowserRouter>
+          <div>
+              <Switch>
+                  <Route
+                      path="/home"
+                      component={Home} />
+                  <Route
+                      exact
+                      path="/adminlogin"
+                      component={LoginAdmin} />
+              </Switch>
+          </div>
+      </BrowserRouter>
   );
 }
 
