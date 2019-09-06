f227298c0b6d7a88804805c677c951a6a55e84fc
Micaela <alonso.mica@hotmail.com>
2019-09-06 17:44:20 -0300

    Saca las card de la pag de admin para en el futuro hacer login

--- frontend-dolce-margarita/src/componentes/loginAdmin/LoginAdmin.js
+++ frontend-dolce-margarita/src/componentes/loginAdmin/LoginAdmin.js
@@ -6,35 +6,7 @@ export default class LoginAdmin extends React.Component {
     render() {
         return (
             <div className="home-page">
-                <div className="container home-title-container">
-                    <h1 className="title"> ¿Qué vas a cargar?</h1>
-                </div>
-
-                <div className="container cards-container">
-                    <div className="columns">
-                        <div className="column">
-                            <div className="card">
-                                <div className="card-content">
-                                    <h1>Figura</h1>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="column">
-                            <div className="card">
-                                <div className="card-content">
-                                    <h1>Huevo</h1>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="column">
-                            <div className="card">
-                                <div className="card-content">
-                                    <h1>Bomboneria</h1>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                ACA VAMOS A HACER UN LOGIN PARA EL ADMIN
             </div>
         )
     }
