2c49ce8d771c0e1cd0583f9276d8f4aa399c0392
mariabelenamat <belen.amat29@gmail.com>
2019-09-07 18:46:14 -0300

    Grilla de cartas con los productos y sus datos

--- frontend-dolce-margarita/src/componentes/listadoDeProductos/ListadoDeProductos.js
+++ frontend-dolce-margarita/src/componentes/listadoDeProductos/ListadoDeProductos.js
@@ -3,83 +3,32 @@ import {productos} from "./listado-hasta-que-mica-levante-el-back";
 import React from "react";
 
 export default class ListadoDeProductos extends React.Component {
-
+    renderCarta = (producto) => {
+        return (
+            <div className="flip-card">
+                <div className="flip-card-inner">
+                    <div className="flip-card-front">
+                        <div className="flip-card-content">
+                            <img src={require('./bombones.jpg')} />
+                        </div>
+                        <div className="flip-card-footer">
+                            <p>{producto.nombre}</p>
+                        </div>
+                    </div>
+                    <div className="flip-card-back">
+                        <p>{producto.descripcion}</p>
+                        <p>${producto.precio}</p>
+                        <a className="button is-danger is-outlined">Comprar</a>
+                    </div>
+                </div>
+            </div>
+        )
+    }
     render() {
         return (
             <div className="home-listado-productos">
                 <div className="cartas">
-                    <div className="card">
-                        <div className="card-content"s/>
-                        <div className="card-footer">
-                            <p className="subtitle">FAFAFA</p>
-                        </div>
-                    </div>
-
-                    <div className="card">
-                        <div className="card-content"s/>
-                        <div className="card-footer">
-                            <p className="subtitle">FAFAFA</p>
-                        </div>
-                    </div>
-
-
-                    <div className="card">
-                        <div className="card-content"s/>
-                        <div className="card-footer">
-                            <p className="subtitle">FAFAFA</p>
-                        </div>
-                    </div>
-
-                    <div className="card">
-                        <div className="card-content"s/>
-                        <div className="card-footer">
-                            <p className="subtitle">FAFAFA</p>
-                        </div>
-                    </div>
-
-                    <div className="card">
-                        <div className="card-content"s/>
-                        <div className="card-footer">
-                            <p className="subtitle">FAFAFA</p>
-                        </div>
-                    </div>
-
-                    <div className="card">
-                        <div className="card-content"s/>
-                        <div className="card-footer">
-                            <p className="subtitle">FAFAFA</p>
-                        </div>
-                    </div>
-
-                    <div className="card">
-                        <div className="card-content"s/>
-                        <div className="card-footer">
-                            <p className="subtitle">FAFAFA</p>
-                        </div>
-                    </div>
-
-                    <div className="card">
-                        <div className="card-content"s/>
-                        <div className="card-footer">
-                            <p className="subtitle">FAFAFA</p>
-                        </div>
-                    </div>
-
-                    <div className="card">
-                        <div className="card-content"s/>
-                        <div className="card-footer">
-                            <p className="subtitle">FAFAFA</p>
-                        </div>
-                    </div>
-
-                    <div className="card">
-                        <div className="card-content"s/>
-                        <div className="card-footer">
-                            <p className="subtitle">FAFAFA</p>
-                        </div>
-                    </div>
-
-
+                    {productos.map(this.renderCarta)}
                 </div>
             </div>
         )
