8ef88778795786294bd55759e0fd70da7c91d0a5
mariabelenamat <belen.amat29@gmail.com>
2019-09-07 21:10:40 -0300

    Agrego modal con datos de la compra

--- frontend-dolce-margarita/src/componentes/listadoDeProductos/ListadoDeProductos.js
+++ frontend-dolce-margarita/src/componentes/listadoDeProductos/ListadoDeProductos.js
@@ -1,8 +1,28 @@
 import './listadoDeProductos.scss';
 import {productos} from "./listado-hasta-que-mica-levante-el-back";
 import React from "react";
+import Modal from 'react-modal';
+import ModalArmadoDePedido from "../armadoDePedido/ModalArmadoDePedido";
+
 
 export default class ListadoDeProductos extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            mostrarModalDeCompra: false,
+            nombreProductoSeleccionado: "",
+            precioProductoSeleccionado: ""
+        }
+    }
+
+    showModal = (producto) => {
+        this.setState({ mostrarModalDeCompra: true,
+            nombreProductoSeleccionado: producto.nombre,
+            precioProductoSeleccionado: producto.precio,
+            });
+    };
+
     renderCarta = (producto) => {
         return (
             <div className="flip-card">
@@ -18,7 +38,9 @@ export default class ListadoDeProductos extends React.Component {
                     <div className="flip-card-back">
                         <p className="datos">{producto.descripcion}</p>
                         <p className="datos">${producto.precio}</p>
-                        <a className="button is-danger is-outlined datos">Comprar</a>
+                        <a className="button is-danger is-outlined datos" onClick={() => this.showModal(producto)}>
+                            Comprar
+                        </a>
                     </div>
                 </div>
             </div>
@@ -30,6 +52,13 @@ export default class ListadoDeProductos extends React.Component {
                 <div className="cartas">
                     {productos.map(this.renderCarta)}
                 </div>
+
+                <ModalArmadoDePedido
+                    modalAbierto={this.state.mostrarModalDeCompra}
+                    nombreDelProducto={this.state.nombreProductoSeleccionado}
+                    precioDelProducto={this.state.precioProductoSeleccionado}
+                />
+
             </div>
         )
     }
