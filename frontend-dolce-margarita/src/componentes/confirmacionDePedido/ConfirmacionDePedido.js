a04932152b94e252817868763d029df44cd933dc
mariabelenamat <belen.amat29@gmail.com>
2019-09-08 21:00:12 -0300

    Agrego botones para confirmar o cancelar pedido

--- frontend-dolce-margarita/src/componentes/confirmacionDePedido/ConfirmacionDePedido.js
+++ frontend-dolce-margarita/src/componentes/confirmacionDePedido/ConfirmacionDePedido.js
@@ -3,18 +3,41 @@ import './confirmacion-de-pedido.scss';
 
 
 export default class ConfirmacionDePedido extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            nombreClienteDelPedido: '',
+            emailClientePedido: ''
+        }
+    }
+
     render() {
         return (
             <div className="confirmacion-pedido">
                 <div className="title">
                     <p className="title is-1 is-spaced">Confirmación de pedido</p>
                 </div>
+
                 <div className="card pedido">
                     <header className="card-header">
-                        <p className="title">Titulo del Pedido</p>
-                    </header>
-                </div>
+                        <p className="card-header-title">
+                            Component
+                        </p>
 
+                    </header>
+                    <div className="card-content">
+                        <div className="content">
+                            <div>
+                                <p>Cantidad: 5</p>
+                            </div>
+                        </div>
+                    </div>
+                    <footer className="card-footer">
+                        <button className="button is-success" onClick={this.continuar}>Continuar</button>
+                        <button className="button is-danger" onClick={this.props.onClose}>Cancelar</button>
+                    </footer>
+                </div>
             </div>
         )
     }
