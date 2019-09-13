06c07cc212be626e308b0b9043e3468786623f65
mariabelenamat <belen.amat29@gmail.com>
2019-09-12 21:11:40 -0300

    agrego test de verificacion de cantidad y precio total no negativos

--- backend/app/models/pedido.rb
+++ backend/app/models/pedido.rb
@@ -1,4 +1,7 @@
 class Pedido < ActiveRecord::Base
   validates_presence_of :email_cliente, :nombre_cliente, :producto, :telefono_cliente, :tipo_chocolate, :lugar_retiro, :cantidad, :precio_total
+  validates :cantidad, numericality: { greater_than: 0 }
+  validates :precio_total, numericality: { greater_than: 0 }
+
   has_one :producto
 end
--- backend/spec/models/pedido_spec.rb
+++ backend/spec/models/pedido_spec.rb
@@ -28,5 +28,41 @@ RSpec.describe Pedido, type: :model do
     end
   end
 
+  context 'un pedido con una cantidad de productos negativa' do
+    let(:atributos) do
+      {
+          producto: producto,
+          email_cliente: 'belen.amat@gmail.com',
+          telefono_cliente: 1159962745,
+          nombre_cliente: 'Belen Amat',
+          tipo_chocolate: 'blanco',
+          cantidad: -3,
+          precio_total: 300,
+          lugar_retiro: 'Berazategui'
+      }
+    end
+    it 'no es valido' do
+      expect(pedido).not_to be_valid
+    end
+  end
+
+  context 'un pedido con un precio total negativo' do
+    let(:atributos) do
+      {
+          producto: producto,
+          email_cliente: 'belen.amat@gmail.com',
+          telefono_cliente: 1159962745,
+          nombre_cliente: 'Belen Amat',
+          tipo_chocolate: 'blanco',
+          cantidad: 3,
+          precio_total: -300,
+          lugar_retiro: 'Berazategui'
+      }
+    end
+    it 'no es valido' do
+      expect(pedido).not_to be_valid
+    end
+  end
 end
+
 
