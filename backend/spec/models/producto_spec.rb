9448016e4b6715c3778cc70dd5fb2e21328c6d99
mariabelenamat <belen.amat29@gmail.com>
2019-09-17 22:13:32 -0300

    Arreglo spec de producto

--- backend/spec/models/producto_spec.rb
+++ backend/spec/models/producto_spec.rb
@@ -4,13 +4,14 @@ RSpec.describe Producto, type: :model do
   let(:producto) do
     Producto.create(atributos)
   end
-  context 'cuando tiene nombre, precio, peso en gramos y descripcion' do
+  context 'cuando tiene nombre, precio, molde, peso en gramos y descripcion' do
     let(:atributos) do
       {
           nombre: 'conejo rosa',
           precio: 130,
           peso_en_gramos: 200,
-          descripcion: 'conejito de chocolate'
+          descripcion: 'conejito de chocolate',
+          molde: 'figura'
       }
     end
     it 'es valido' do
@@ -23,7 +24,8 @@ RSpec.describe Producto, type: :model do
       {
           precio: 130,
           peso_en_gramos: 200,
-          descripcion: 'conejito de chocolate'
+          descripcion: 'conejito de chocolate',
+          molde: 'figura'
       }
     end
     it 'no es valido' do
@@ -36,7 +38,8 @@ RSpec.describe Producto, type: :model do
       {
           nombre: 'conejo rosa',
           peso_en_gramos: 200,
-          descripcion: 'conejito de chocolate'
+          descripcion: 'conejito de chocolate',
+          molde: 'figura'
       }
     end
     it 'no es valido' do
@@ -49,7 +52,8 @@ RSpec.describe Producto, type: :model do
       {
           nombre: 'conejo rosa',
           precio: 130,
-          descripcion: 'conejito de chocolate'
+          descripcion: 'conejito de chocolate',
+          molde: 'figura'
       }
     end
     it 'no es valido' do
@@ -63,6 +67,21 @@ RSpec.describe Producto, type: :model do
           nombre: 'conejo rosa',
           precio: 130,
           peso_en_gramos: 200,
+          molde: 'figura'
+      }
+    end
+    it 'no es valido' do
+      expect(producto).not_to be_valid
+    end
+  end
+
+  context 'cuando no tiene molde' do
+    let(:atributos) do
+      {
+          nombre: 'conejo rosa',
+          precio: 130,
+          peso_en_gramos: 200,
+          descripcion: 'conejito de chocolate'
       }
     end
     it 'no es valido' do
