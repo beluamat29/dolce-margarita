1f97a16f80e9db3d478310f2f01eae7b544f7959
mariabelenamat <belen.amat29@gmail.com>
2019-09-21 18:30:16 -0300

    Se mueve la dependencia de cors a donde corresponde. Se crea el seed con varios productos

--- backend/app/controllers/productos_controller.rb
+++ backend/app/controllers/productos_controller.rb
@@ -1,27 +1,6 @@
 class ProductosController < ApplicationController
   protect_from_forgery with: :null_session
 
-  FIGURAS = [
-      {
-          nombre: "Chupetin de conejo",
-          descripcion: "Figura rellena de gomitas y confites de colores!",
-          peso: 90,
-          precio: 432
-      },
-      {
-          nombre: "Chupetin de conejo",
-          descripcion: "Figura rellena de gomitas y confites de colores!",
-          peso: 500,
-          precio: 100
-      },
-      {
-          nombre: "Chupetin de conejo",
-          descripcion: "Figura rellena de gomitas y confites de colores!",
-          peso: 90,
-          precio: 130
-      }
-  ].freeze
-
   HUEVOS = [
       {
           nombre: "Gallina rellena",
@@ -43,33 +22,12 @@ class ProductosController < ApplicationController
       }
   ].freeze
 
-  BOMBONES = [
-      {
-          nombre: "Balde de huevitos",
-          descripcion: "Balde colorido lleno de pequeños huevos",
-          peso: 300,
-          precio: 90
-      },
-      {
-          nombre: "Balde de huevitos",
-          descripcion: "Balde colorido lleno de pequeños huevos",
-          peso: 300,
-          precio: 230
-      },
-      {
-          nombre: "Balde de huevitos",
-          descripcion: "Balde colorido lleno de pequeños huevos",
-          peso: 300,
-          precio: 190
-      }
-  ].freeze
-
   def show_figuras
     render json: Producto.where(molde: "figura"), status: :ok
   end
 
   def show_bombones
-    render json: BOMBONES, status: :ok
+    render json: Producto.where(molde: "bomboneria"), status: :ok
   end
 
   def show_huevos
