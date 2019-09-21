647c4c291aedf0943d4d408786e6740c27579ec0
mariabelenamat <belen.amat29@gmail.com>
2019-09-20 21:26:44 -0300

    Agrego vista para el login del admin

--- frontend-dolce-margarita/src/componentes/loginAdmin/LoginAdmin.js
+++ frontend-dolce-margarita/src/componentes/loginAdmin/LoginAdmin.js
@@ -6,7 +6,26 @@ export default class LoginAdmin extends React.Component {
     render() {
         return (
             <div className="home-page">
-                ACA VAMOS A HACER UN LOGIN PARA EL ADMIN
+                <div className="home-title-container">
+                    <p className="title">Logeate como administrador</p>
+                </div>
+
+                <div className="fields-container">
+                    <div className="field">
+                        <div className="control">
+                            <input className="input is-primary" type="text" placeholder="Usuario administrador"/>
+                        </div>
+                    </div>
+                    <div className="field">
+                        <div className="control">
+                            <input className="input is-primary" type="text" placeholder="Contraseña"/>
+                        </div>
+                    </div>
+
+                    <a className="button is-danger datos">
+                        Ingresar
+                    </a>
+                </div>
             </div>
         )
     }
