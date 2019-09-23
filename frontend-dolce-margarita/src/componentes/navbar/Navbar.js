fb22f7e94bf38f71221ee597bfaba0503ed4f157
Micaela <alonso.mica@hotmail.com>
2019-09-23 09:43:20 -0300

    Agrega margarita para el navbar y estilos

--- frontend-dolce-margarita/src/componentes/navbar/Navbar.js
+++ frontend-dolce-margarita/src/componentes/navbar/Navbar.js
@@ -5,49 +5,22 @@ const Navbar = () => {
   return (
     <nav className="navbar" role="navigation" aria-label="main navigation">
       <div className="navbar-brand">
-        <a className="navbar-item" href="https://bulma.io">
-          <img src={require('../../assets/margarita.png')} width="112" height="28"/>
-        </a>
-
-        <a role="button" className="navbar-burger burger" aria-label="menu" aria-expanded="false"
-           data-target="navbarBasicExample">
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-        </a>
+        <img src={require('../../assets/margarita.png')} width="112" height="28"/>
       </div>
 
       <div id="navbarBasicExample" className="navbar-menu">
         <div className="navbar-start">
           <a className="navbar-item">
-            Home
+            Figuras
           </a>
 
           <a className="navbar-item">
-            Documentation
+            Bombonería
           </a>
 
-          <div className="navbar-item has-dropdown is-hoverable">
-            <a className="navbar-link">
-              More
-            </a>
-
-            <div className="navbar-dropdown">
-              <a className="navbar-item">
-                About
-              </a>
-              <a className="navbar-item">
-                Jobs
-              </a>
-              <a className="navbar-item">
-                Contact
-              </a>
-              <hr className="navbar-divider"/>
-                <a className="navbar-item">
-                  Report an issue
-                </a>
-            </div>
-          </div>
+          <a className="navbar-item">
+            Huevos
+          </a>
         </div>
       </div>
     </nav>
