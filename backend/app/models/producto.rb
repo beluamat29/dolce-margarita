bd2a268c045b0b566d2f78a7b7c37491050bc86a
mariabelenamat <belen.amat29@gmail.com>
2019-09-24 22:04:47 -0300

    Agrego validacion del campo tamaño en caso de que sea un huevo

--- backend/app/models/producto.rb
+++ backend/app/models/producto.rb
@@ -1,6 +1,6 @@
 class Producto < ActiveRecord::Base
   validates_presence_of :nombre, :precio, :peso_en_gramos, :descripcion, :molde
-
+  validates_presence_of :tamanio, if: :molde_es_huevo
   def self.agregar_figura(params)
     self.create(nombre: params[:nombre],
                 precio: params[:precio],
@@ -18,5 +18,9 @@ class Producto < ActiveRecord::Base
                 descripcion: params[:descripcion],
                 tamanio: params[:tamanio]
     )
+  end
+
+  def molde_es_huevo
+    molde.eql? 'huevo'
   end
 end
