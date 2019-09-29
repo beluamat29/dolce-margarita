0f76070ed90203dee753467bae2ac2cb28456df8
mariabelenamat <belen.amat29@gmail.com>
2019-09-29 16:51:37 -0300

    Chequeo que un usuario se cree por default como NO admin

--- backend/spec/controllers/user_controller_spec.rb
+++ backend/spec/controllers/user_controller_spec.rb
@@ -16,15 +16,20 @@ RSpec.describe UserController, type: :request do
         }
       end
 
-      it 'agrega el usuario con los parametros dados' do
+      it 'agrega el usuario con los parametros dados, devuelve CREATED y el usuario no es admin' do
         expect {post '/usuarios', params}.to change(User, :count).by(+1)
         expect(response).to have_http_status :created
 
         expect(JSON.parse(response.body)["nombre"]).to eq(params[:nombre])
         expect(JSON.parse(response.body)["apellido"]).to eq(params[:apellido])
         expect(JSON.parse(response.body)["email"]).to eq(params[:email])
+        expect(JSON.parse(response.body)["admin"]).to be_falsey
+
         expect(usuario_se_creo_con_contrasenia(params[:password])).to be_truthy
       end
     end
+
+    context 'un usuario puede ser admin si se lo indica en los parametros'
+
   end
 end
