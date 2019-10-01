93c4e9be53be3fb198a7d828299e8b9809a1ba45
mariabelenamat <belen.amat29@gmail.com>
2019-09-30 21:21:15 -0300

    Agrego tests que habia borrado sin querer!

--- backend/spec/controllers/productos_controller_spec.rb
+++ backend/spec/controllers/productos_controller_spec.rb
@@ -23,6 +23,51 @@ RSpec.describe ProductosController, type: :request do
         expect(JSON.parse(response.body)["molde"]).to eq(params[:molde])
       end
     end
+
+    context 'cuando se recibe bomboneria' do
+      let(:params) do
+        {
+            nombre: "Caja de bombones x 500gr",
+            precio: 700,
+            peso_en_gramos: 500,
+            descripcion: "Caja de bombones",
+            molde: "bomboneria"
+        }
+      end
+
+      it 'agrega los bombones a la lista de figuras, tiene estado created y devuelve los bombones creados' do
+        expect { post '/productos', params}.to change(Producto, :count).by(+1)
+        expect(response).to have_http_status :created
+        expect(JSON.parse(response.body)["nombre"]).to eq(params[:nombre])
+        expect(JSON.parse(response.body)["precio"]).to eq(params[:precio])
+        expect(JSON.parse(response.body)["peso_en_gramos"]).to eq(params[:peso_en_gramos])
+        expect(JSON.parse(response.body)["descripcion"]).to eq(params[:descripcion])
+        expect(JSON.parse(response.body)["molde"]).to eq(params[:molde])
+      end
+    end
+
+    context 'cuando se recibe un huevo' do
+      let(:params) do
+        {
+            nombre: "Huevo",
+            precio: 700,
+            peso_en_gramos: 500,
+            descripcion: "Huevo relleno",
+            molde: "huevo",
+            tamanio: 14
+        }
+      end
+
+      it 'agrega el huevo a la lista de productos, tiene estado created y devuelve el huevo creado' do
+        expect { post '/productos', params}.to change(Producto, :count).by(+1)
+        expect(response).to have_http_status :created
+        expect(JSON.parse(response.body)["nombre"]).to eq(params[:nombre])
+        expect(JSON.parse(response.body)["precio"]).to eq(params[:precio])
+        expect(JSON.parse(response.body)["peso_en_gramos"]).to eq(params[:peso_en_gramos])
+        expect(JSON.parse(response.body)["descripcion"]).to eq(params[:descripcion])
+        expect(JSON.parse(response.body)["molde"]).to eq(params[:molde])
+      end
+    end
   end
 
 end
