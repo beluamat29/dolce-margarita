9502e7e9d1d9ba3172f59f7eeb169f21cf33c82a
mariabelenamat <belen.amat29@gmail.com>
2019-10-04 17:16:51 -0300

    Mockeo el service que obtiene los pedidos actuales

--- frontend-dolce-margarita/src/componentes/indexPedidos/IndexPedidos.js
+++ frontend-dolce-margarita/src/componentes/indexPedidos/IndexPedidos.js
@@ -1,7 +1,27 @@
 import React from 'react';
 import './indexPedidos.scss';
+import servicioPedidos from "../../servicios/ServicioPedidos";
 
 export default class IndexPedidos extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            pedidos: []
+        }
+    }
+
+    componentDidMount() {
+        this.actualizarPedidos(this.obtenerPedidos());
+    }
+
+    obtenerPedidos = () => {
+        return servicioPedidos.pedidosActuales();
+    }
+
+    actualizarPedidos = (pedidos) => {
+        this.setState({pedidos});
+    }
 
     render() {
         return (
