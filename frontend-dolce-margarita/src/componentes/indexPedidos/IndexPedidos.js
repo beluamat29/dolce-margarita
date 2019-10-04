498820837f982e66df9e1cc0968031ad4716efce
mariabelenamat <belen.amat29@gmail.com>
2019-10-04 17:45:27 -0300

    Se muestra el listado de pedidos

--- frontend-dolce-margarita/src/componentes/indexPedidos/IndexPedidos.js
+++ frontend-dolce-margarita/src/componentes/indexPedidos/IndexPedidos.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import './indexPedidos.scss';
 import servicioPedidos from "../../servicios/ServicioPedidos";
+import Navbar from "../navbar/Navbar";
+import ModalArmadoDePedido from "../armadoDePedido/ModalArmadoDePedido";
 
 export default class IndexPedidos extends React.Component {
     constructor(props) {
         super(props);
 
         this.state = {
+            pedido: {
+                nombre_producto: "Conejito",
+                peso_en_gramos: 235,
+                cantidad: 3,
+                tipo_chocolate: 'semi amargo',
+                precio_total: 350.00,
+                nombre_cliente: 'Lucas Avalos',
+                email_cliente: 'eltumba@gmail.com',
+                telefono_cliente: 1122445566,
+                lugar_retiro: 'Calle 6 5047'
+            },
             pedidos: []
         }
     }
@@ -23,10 +36,19 @@ export default class IndexPedidos extends React.Component {
         this.setState({pedidos});
     }
 
+    renderPedido = (pedido) => {
+        return (<div className='row'>
+                    <p className="nombre-cliente">{pedido.nombre_cliente}</p>
+                    <p className="pedido">{pedido.nombre_producto + ' ' + pedido.peso_en_gramos + "grs x " + pedido.cantidad + 'u'}</p>
+                </div>);
+    }
+
     render() {
         return (
-            <div className="index-pedidos-home ">
-                <p>holis</p>
+            <div className="index-pedidos-home">
+                <div className="rows">
+                    {this.state.pedidos.map(this.renderPedido)}
+                </div>
             </div>
         )
     }
