3c8ef7f1544ea0ee9c2d9bdc7cf708cfd26c8fd9
Micaela <alonso.mica@hotmail.com>
2019-10-14 19:40:53 -0300

    Agrega test para caso bad request

--- backend/app/controllers/pedidos_controller.rb
+++ backend/app/controllers/pedidos_controller.rb
@@ -4,7 +4,7 @@ class PedidosController < ApplicationController
   def create
     pedido_parcial = params[:pedido_parcial]
     producto = Producto.find_by(id: pedido_parcial[:producto][:id])
-    pedido = Pedido.create!(
+    @pedido = Pedido.create!(
         producto: producto,
         cantidad: pedido_parcial[:cantidad],
         tipo_chocolate: pedido_parcial[:tipo_chocolate],
@@ -14,11 +14,23 @@ class PedidosController < ApplicationController
         telefono_cliente: params[:telefono_cliente],
         lugar_retiro: params[:lugar_retiro]
     )
-    render json: pedido, status: :created
+
+    render json: @pedido, status: :created, nothing: true
   end
 
   private
   def pedido_params
-    params.permit(:pedido_parcial, :nombre_cliente, :email_cliente, :telefono_cliente, :lugar_retiro)
+    begin
+      params.require(:pedido_parcial)
+      params.require(:nombre_cliente)
+      params.require(:email_cliente)
+      params.require(:telefono_cliente)
+      params.require(:lugar_retiro)
+
+
+      params.permit(:pedido_parcial, :nombre_cliente, :email_cliente, :telefono_cliente, :lugar_retiro)
+    rescue
+      render status: :bad_request, nothing: true
+    end
   end
 end
