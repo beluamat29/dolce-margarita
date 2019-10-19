8eec7e55707b17ab27b26b7fcdc3aa907823a122
mariabelenamat <belen.amat29@gmail.com>
2019-10-19 18:56:48 -0300

    Cuando se crea un pedido, su estado default es EN ESPERA

--- backend/app/models/pedido.rb
+++ backend/app/models/pedido.rb
@@ -1,4 +1,6 @@
 class Pedido < ActiveRecord::Base
+
+  EN_ESPERA = 'EN ESPERA'
   validates_presence_of :email_cliente, :nombre_cliente, :producto, :telefono_cliente, :tipo_chocolate, :lugar_retiro, :cantidad, :precio_total
   validates :cantidad, numericality: { greater_than: 0 }
   validates :precio_total, numericality: { greater_than: 0 }
